02d2d0236e8eee69d43f09f73352fb096cca4da9
Dan Rusei <dan.rusei@gmail.com>
2024-04-18 14:23:40 +0300

    crates initial structure

--- src/broker.rs
+++
@@ -1,1 +0,0 @@
-pub(crate) mod danube_service;
--- src/broker/danube_service.rs
+++
@@ -1,8 +0,0 @@
-struct DanubeService {
-    config: ServiceConfiguration,
-    advertised_address: String,
-}
-
-struct ServiceConfiguration {
-    todo: String,
-}
