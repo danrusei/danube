3107f314828bc2258e873f89784629184f354eb4
Dan Rusei <dan.rusei@gmail.com>
2024-04-19 11:43:20 +0300

    seting up the axum http server

--- danube-controller/src/main.rs
+++ danube-controller/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod service;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // run the HTTP server
+    service::run();
 }
