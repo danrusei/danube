b79f39d90df593fa2362ca9f224451d98ef36acb
Dan Rusei <dan.rusei@gmail.com>
2024-04-23 15:21:29 +0300

    client to broker connection via grpc

--- danube-broker/build.rs
+++ danube-broker/build.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    tonic_build::compile_protos("../proto/DanubeApi.proto")?;
+    tonic_build::configure()
+        .out_dir("../proto")
+        .compile(&["../proto/DanubeApi.proto"], &["../proto"])?;
     Ok(())
 }
--- danube-broker/src/main.rs
+++ danube-broker/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use proto::danube_server::{Danube, DanubeServer};
+use tonic::transport::Server;
+use tonic::{Request, Response};
+
+mod proto {
+    tonic::include_proto!("danube");
+}
+
+#[derive(Debug, Default)]
+struct DanubeService {}
+
+#[tonic::async_trait]
+impl Danube for DanubeService {
+    async fn create_producer(
+        &self,
+        request: Request<proto::ProducerRequest>,
+    ) -> Result<Response<proto::ProducerResponse>, tonic::Status> {
+        let req = request.get_ref();
+
+        println!(
+            "{} {} {} {}",
+            req.request_id, req.producer_id, req.producer_name, req.topic,
+        );
+
+        let response = proto::ProducerResponse {
+            request_id: req.request_id,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+    async fn subscribe(
+        &self,
+        request: Request<proto::ConsumerRequest>,
+    ) -> Result<Response<proto::ConsumerResponse>, tonic::Status> {
+        todo!()
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:6650".parse()?;
+
+    let danube = DanubeService::default();
+
+    Server::builder()
+        .add_service(DanubeServer::new(danube))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
