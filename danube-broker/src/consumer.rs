ba3388aaa66a4fccbc279525e6e063ab7cbb6e8d
Dan Rusei <dan.rusei@gmail.com>
2024-05-15 19:47:51 +0300

    consumer strucuture

--- danube-broker/src/consumer.rs
+++ danube-broker/src/consumer.rs
@@ -1,2 +1,49 @@
+use anyhow::Result;
+use bytes::Bytes;
+
+use crate::subscription::{self, Subscription};
+
+/// A Consumer is a consumer connected and associated with a Subscription.
 #[derive(Debug, Default)]
-pub(crate) struct Consumer {}
+pub(crate) struct Consumer {
+    consumer_id: u64,
+    consumer_name: String,
+}
+
+impl Consumer {
+    pub(crate) fn new(
+        subscription: Subscription,
+        topic_name: String,
+        consumer_name: String,
+        consumer_id: u64,
+    ) -> Self {
+        Consumer {
+            consumer_id,
+            consumer_name,
+        }
+    }
+
+    // Dispatch a list of entries to the consumer.
+    pub(crate) async fn send_messages(entries: Vec<Bytes>, batch_size: u32) -> Result<()> {
+        let unacked_messages = entries.len();
+        todo!()
+    }
+
+    // Close the consumer if: a. the connection is dropped
+    // b. all messages were delivered and there are no pending message acks, graceful close connection
+    pub(crate) fn close(&self) -> Result<()> {
+        // subscription.remove_consumer(self)
+        todo!()
+    }
+
+    // Unsubscribe consumer from the Subscription
+    pub(crate) fn unsubscribe(&self) -> Result<()> {
+        // subscription.unsubscribe(self)
+        todo!()
+    }
+
+    // acked message from client
+    pub(crate) fn message_acked() -> Result<()> {
+        todo!()
+    }
+}
