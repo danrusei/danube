6b06889f01e6fd6f7c3a5821e6fd5ff90d52e586
Dan Rusei <dan.rusei@gmail.com>
2024-06-14 16:33:14 +0300

    adding the conversions from client to proto schema

--- danube-client/src/schema.rs
+++ danube-client/src/schema.rs
@@ -1,6 +1,11 @@
-use crate::proto::{schema::TypeSchema, Schema as ProtoSchema};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::convert::TryFrom;
 
-#[derive(Debug, Clone)]
+use crate::proto::{schema::TypeSchema as ProtoTypeSchema, Schema as ProtoSchema};
+
+// Define the enum with serde attributes for (de)serialization
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum SchemaType {
     Bytes,
     String,
@@ -8,18 +13,31 @@ pub enum SchemaType {
     Json(String), // JSON schema described by a string
 }
 
-impl From<SchemaType> for TypeSchema {
+// Implement conversions from SchemaType to ProtoTypeSchema
+impl From<SchemaType> for ProtoTypeSchema {
     fn from(schema_type: SchemaType) -> Self {
         match schema_type {
-            SchemaType::Bytes => TypeSchema::Bytes,
-            SchemaType::String => TypeSchema::String,
-            SchemaType::Int64 => TypeSchema::Int64,
-            SchemaType::Json(_) => TypeSchema::Json,
+            SchemaType::Bytes => ProtoTypeSchema::Bytes,
+            SchemaType::String => ProtoTypeSchema::String,
+            SchemaType::Int64 => ProtoTypeSchema::Int64,
+            SchemaType::Json(_) => ProtoTypeSchema::Json,
         }
     }
 }
 
-#[derive(Debug, Clone)]
+// Implement conversions from ProtoTypeSchema to SchemaType
+impl From<ProtoTypeSchema> for SchemaType {
+    fn from(proto_schema: ProtoTypeSchema) -> Self {
+        match proto_schema {
+            ProtoTypeSchema::Bytes => SchemaType::Bytes,
+            ProtoTypeSchema::String => SchemaType::String,
+            ProtoTypeSchema::Int64 => SchemaType::Int64,
+            ProtoTypeSchema::Json => SchemaType::Json(String::new()),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Schema {
     name: String,
     schema_data: Option<Vec<u8>>,
@@ -39,11 +57,33 @@ impl Schema {
         }
     }
 
-    pub fn to_proto(&self) -> ProtoSchema {
+    // Function to convert Schema to serde_json::Value
+    fn schema_to_json_value(proto_schema: &ProtoSchema) -> Value {
+        let schema: Schema = proto_schema.into();
+        serde_json::to_value(&schema).unwrap()
+    }
+}
+
+// Implement a conversion method from ProtoSchema to Schema
+impl From<&ProtoSchema> for Schema {
+    fn from(proto_schema: &ProtoSchema) -> Self {
+        let type_schema =
+            ProtoTypeSchema::try_from(proto_schema.type_schema).expect("Invalid type schema");
+        Schema {
+            name: proto_schema.name.clone(),
+            schema_data: Some(proto_schema.schema_data.clone()),
+            type_schema: type_schema.into(),
+        }
+    }
+}
+
+// Implement a conversion method from Schema to ProtoSchema
+impl From<&Schema> for ProtoSchema {
+    fn from(schema: &Schema) -> Self {
         ProtoSchema {
-            name: self.name.clone(),
-            schema_data: self.schema_data.clone().unwrap_or_default(),
-            type_schema: TypeSchema::from(self.type_schema.clone()) as i32,
+            name: schema.name.clone(),
+            schema_data: schema.schema_data.clone().unwrap(),
+            type_schema: ProtoTypeSchema::from(schema.type_schema.clone()).into(),
         }
     }
 }
