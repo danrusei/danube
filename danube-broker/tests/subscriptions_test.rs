19308fb6b5207844d0a335f7e4427e2c8c328a7d
Dan Rusei <dan.rusei@gmail.com>
2024-07-16 18:16:35 +0300

    work in progress, subscription tests

--- danube-broker/tests/subscriptions_test.rs
+++ danube-broker/tests/subscriptions_test.rs
@@ -2,51 +2,161 @@ extern crate danube_client;
 extern crate futures_util;
 
 use anyhow::Result;
-use danube_client::{DanubeClient, SchemaType, SubType};
+use danube_client::{Consumer, DanubeClient, Producer, SchemaType, SubType};
 use futures_util::stream::StreamExt;
+use once_cell::sync::OnceCell;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio::time::{sleep, timeout, Duration};
 
-#[tokio::test]
-async fn test_exclusive_subscription() -> Result<()> {
-    let client = DanubeClient::builder()
-        .service_url("http://[::1]:6650")
-        .build()
-        .unwrap();
+struct TestSetup {
+    producer: Arc<Mutex<Producer>>,
+    client: Arc<DanubeClient>,
+    topic: String,
+}
+
+static CLIENT: OnceCell<Arc<DanubeClient>> = OnceCell::new();
+static PRODUCER: OnceCell<Arc<Mutex<Producer>>> = OnceCell::new();
+
+async fn setup() -> Result<TestSetup> {
+    let client = CLIENT
+        .get_or_init(|| {
+            Arc::new(
+                DanubeClient::builder()
+                    .service_url("http://[::1]:6650")
+                    .build()
+                    .unwrap(),
+            )
+        })
+        .clone();
 
     let topic = "/default/test_topic".to_string();
 
-    // create the producer
-    let mut producer = client
-        .new_producer()
-        .with_topic(topic.clone())
-        .with_name("test_producer1")
-        .with_schema("my_app".into(), SchemaType::String)
-        .build();
+    // Initialize the producer only once
+    if PRODUCER.get().is_none() {
+        let mut producer = client
+            .new_producer()
+            .with_topic(topic.clone())
+            .with_name("test_producer1")
+            .with_schema("my_app".into(), SchemaType::String)
+            .build();
 
-    let _prod_id = producer.create().await?;
+        producer.create().await?;
 
-    let _message_id = producer.send("Hello Danube".as_bytes().into()).await?;
+        PRODUCER.set(Arc::new(Mutex::new(producer))).unwrap();
+    }
 
-    // Create consumer with exclusive subscription
+    let producer = PRODUCER.get().unwrap().clone();
+
+    Ok(TestSetup {
+        producer,
+        client,
+        topic,
+    })
+}
+
+async fn setup_consumer(
+    client: Arc<DanubeClient>,
+    topic: &str,
+    consumer_name: &str,
+    sub_type: SubType,
+) -> Result<Consumer> {
     let mut consumer = client
         .new_consumer()
-        .with_topic(topic.clone())
-        .with_consumer_name("test_consumer")
-        .with_subscription("test_subscription")
-        .with_subscription_type(SubType::Exclusive)
+        .with_topic(topic.to_string())
+        .with_consumer_name(consumer_name.to_string())
+        .with_subscription(format!("test_subscription_{}", consumer_name))
+        .with_subscription_type(sub_type)
         .build();
 
-    // Subscribe to the topic
-    let _consumer_id = consumer.subscribe().await?;
+    // Ensure the consumer is connected and subscribed
+    consumer.subscribe().await?;
+    Ok(consumer)
+}
+
+#[tokio::test]
+async fn test_exclusive_subscription() -> Result<()> {
+    let setup = setup().await?;
+
+    let mut consumer = setup_consumer(
+        setup.client.clone(),
+        &setup.topic,
+        "test_consumer_exclusive",
+        SubType::Exclusive,
+    )
+    .await?;
+    let producer = setup.producer.lock().await;
 
     // Start receiving messages
     let mut message_stream = consumer.receive().await?;
 
-    if let Some(message) = message_stream.next().await {
-        let msg = message.unwrap();
-        let payload = String::from_utf8(msg.messages).unwrap();
-        assert_eq!(payload, "Hello Danube");
-        // consumer.ack(&msg).await.unwrap();
-    }
+    // Produce a message after the consumer has subscribed
+    let _message_id = producer.send("Hello Danube".as_bytes().into()).await?;
+    println!("Message sent");
+
+    // Add a timeout to avoid blocking indefinitely
+    let receive_future = async {
+        if let Some(message) = message_stream.next().await {
+            let msg = message.unwrap();
+            let payload = String::from_utf8(msg.messages).unwrap();
+            assert_eq!(payload, "Hello Danube");
+            println!("Message received: {}", payload);
+            // consumer.ack(&msg).await.unwrap();
+        } else {
+            println!("No message received");
+        }
+    };
+
+    let result = timeout(Duration::from_secs(10), receive_future).await;
+    assert!(
+        result.is_ok(),
+        "Test timed out while waiting for the message"
+    );
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_shared_subscription() -> Result<()> {
+    // find a better way to wait for initial setup
+    sleep(Duration::from_secs(4)).await;
+
+    let setup = setup().await?;
+
+    let mut consumer = setup_consumer(
+        setup.client.clone(),
+        &setup.topic,
+        "test_consumer_shared",
+        SubType::Shared,
+    )
+    .await?;
+    let producer = setup.producer.lock().await;
+
+    // Start receiving messages
+    let mut message_stream = consumer.receive().await?;
+
+    // Produce a message after the consumer has subscribed
+    let _message_id = producer.send("Hello Danube".as_bytes().into()).await?;
+    println!("Message sent");
+
+    // Add a timeout to avoid blocking indefinitely
+    let receive_future = async {
+        if let Some(message) = message_stream.next().await {
+            let msg = message.unwrap();
+            let payload = String::from_utf8(msg.messages).unwrap();
+            assert_eq!(payload, "Hello Danube");
+            println!("Message received: {}", payload);
+            // consumer.ack(&msg).await.unwrap();
+        } else {
+            println!("No message received");
+        }
+    };
+
+    let result = timeout(Duration::from_secs(10), receive_future).await;
+    assert!(
+        result.is_ok(),
+        "Test timed out while waiting for the message"
+    );
 
     Ok(())
 }
