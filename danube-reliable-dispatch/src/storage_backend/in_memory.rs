881e6efe32ae178ab9aedfa9357a1b91e52be240
Dan Rusei <dan.rusei@gmail.com>
2024-12-26 16:26:18 +0200

    implement InMemory storage

--- danube-reliable-dispatch/src/storage_backend/in_memory.rs
+++ danube-reliable-dispatch/src/storage_backend/in_memory.rs
@@ -3,7 +3,11 @@ use dashmap::DashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
-use crate::{errors::Result, storage_backend::StorageBackend, topic_storage::Segment};
+use crate::{
+    errors::{ReliableDispatchError, Result},
+    storage_backend::StorageBackend,
+    topic_storage::Segment,
+};
 
 #[derive(Debug)]
 pub struct InMemoryStorage {
@@ -20,16 +24,37 @@ impl InMemoryStorage {
 
 #[async_trait]
 impl StorageBackend for InMemoryStorage {
-    async fn get_segment(&self, _id: usize) -> Result<Option<Arc<RwLock<Segment>>>> {
-        todo!()
+    async fn get_segment(&self, id: usize) -> Result<Option<Arc<RwLock<Segment>>>> {
+        match self.segments.get(&id) {
+            Some(segment) => Ok(Some(segment.clone())),
+            None => Err(ReliableDispatchError::SegmentError(
+                "Unable to find the segment".to_string(),
+            )),
+        }
     }
-    async fn put_segment(&self, _id: usize, _segment: Arc<RwLock<Segment>>) -> Result<()> {
-        todo!()
+
+    async fn put_segment(&self, id: usize, segment: Arc<RwLock<Segment>>) -> Result<()> {
+        match self.segments.insert(id, segment) {
+            Some(_) => Ok(()),
+            None => Err(ReliableDispatchError::SegmentError(
+                "Unable to create the segment".to_string(),
+            )),
+        }
     }
-    async fn remove_segment(&self, _id: usize) -> Result<()> {
-        todo!()
+
+    async fn remove_segment(&self, id: usize) -> Result<()> {
+        match self.segments.remove(&id) {
+            Some(_) => Ok(()),
+            None => Err(ReliableDispatchError::SegmentError(
+                "Unable to delete the segment".to_string(),
+            )),
+        }
     }
-    async fn contains_segment(&self, _id: usize) -> Result<bool> {
-        todo!()
+
+    async fn contains_segment(&self, id: usize) -> Result<bool> {
+        match self.segments.contains_key(&id) {
+            true => Ok(true),
+            false => Ok(false),
+        }
     }
 }
